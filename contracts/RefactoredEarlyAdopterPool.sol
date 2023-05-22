0302a2c11b47afb96c5699ba9aa87e3dde9b9067
Akshit Choudhary <akshitchoudhary007@gmail.com>
2023-05-23 00:27:27 +0530

    updated natspec comments

--- contracts/RefactoredEarlyAdopterPool.sol
+++ contracts/RefactoredEarlyAdopterPool.sol
@@ -7,37 +7,47 @@ import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
+/**
+ * @title RefactoredEarlyAdopterPool
+ * @dev Contract for a pool where users can deposit supported ERC20 tokens or Ether and earn points based on the amount of time and the amount deposited. Users can claim their points at a later time.
+ */
 contract RefactoredEarlyAdopterPool is Ownable, ReentrancyGuard, Pausable {
     using Math for uint256;
 
     error TokenTransferFailed();
 
-    //--------------------------------------------------------------------------------------
-    //---------------------------------  STATE-VARIABLES  ----------------------------------
-    //--------------------------------------------------------------------------------------
-
-    //After a certain time, claiming funds is not allowed and users will need to simply withdraw
+    // After a certain time, claiming funds is not allowed and users will need to simply withdraw
     uint256 public claimDeadline;
 
-    //Time when depositing closed and will be used for calculating reards
+    // Time when depositing closed and will be used for calculating rewards
     uint256 public endTime;
 
-    IERC20 private immutable _rETH; // 0xae78736Cd615f374D3085123A210448E74Fc6393;
-    IERC20 private immutable _wstETH; // 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
-    IERC20 private immutable _sfrxETH; // 0xac3E018457B222d93114458476f3E3416Abbe38F;
-    IERC20 private immutable _cbETH; // 0xBe9895146f7AF43049ca1c1AE358B0541Ea49704;
+    // Address of the rEth contract : 0xae78736Cd615f374D3085123A210448E74Fc6393
+    IERC20 private immutable _rETH;
 
-    //Future contract which funds will be sent to on claim (Most likely LP)
+    // Address of the wstEth contract : 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0
+    IERC20 private immutable _wstETH;
+
+    // Address ofthe sfrxEth contract : 0xac3E018457B222d93114458476f3E3416Abbe38F
+    IERC20 private immutable _sfrxETH;
+
+    // Address of the cbEth contract : 0xBe9895146f7AF43049ca1c1AE358B0541Ea49704
+    IERC20 private immutable _cbETH;
+
+    // Future contract which funds will be sent to on claim (Most likely LP)
     address public claimReceiverContract;
 
-    //Status of claims, 1 means claiming is open
+    // Status of claims, 1 means claiming is open
     uint8 public claimingOpen;
 
+    // Mapping of user addresses to their deposit information
     mapping(address => UserDepositInfo) public depositInfo;
 
-    //user address => token address = balance
+    // Mapping of user addresses to their token balances
+    // user address => token address => balance
     mapping(address => mapping(IERC20 => uint256)) public userToErc20Balance;
 
+    // Mapping of supported tokens
     mapping(IERC20 => bool) public isSupportedToken;
 
     struct UserDepositInfo {
@@ -46,29 +56,72 @@ contract RefactoredEarlyAdopterPool is Ownable, ReentrancyGuard, Pausable {
         uint256 totalERC20Balance;
     }
 
-    //--------------------------------------------------------------------------------------
-    //-------------------------------------  EVENTS  ---------------------------------------
-    //--------------------------------------------------------------------------------------
+    // Modifier to check if deposit amount is between 0.1 ether and 100 ether
+    modifier onlyCorrectAmount(uint256 _amount) {
+        require(
+            _amount >= 0.1 ether && _amount <= 100 ether,
+            "Incorrect Deposit Amount"
+        );
+        _;
+    }
 
+    // Modifier to check if depositing is open
+    modifier depositingOpen() {
+        require(claimingOpen == 0, "Depositing closed");
+        _;
+    }
+
+    /**
+     * @dev Emitted when an ERC20 deposit is made
+     * @param sender The address of the user making the deposit
+     * @param amount The amount of ERC20 tokens deposited
+     */
     event DepositERC20(address indexed sender, uint256 amount);
+
+    /**
+     * @dev Emitted when an Ether deposit is made
+     * @param sender The address of the user making the deposit
+     * @param amount The amount of Ether deposited
+     */
     event DepositEth(address indexed sender, uint256 amount);
+
+    /**
+     * @dev Emitted when a user withdraws their deposit
+     * @param sender The address of the user withdrawing their deposit
+     */
     event Withdrawn(address indexed sender);
+
+    /**
+     * @dev Emitted when the claim receiver contract address is set
+     * @param receiverAddress The address of the claim receiver contract
+     */
     event ClaimReceiverContractSet(address indexed receiverAddress);
+
+    /**
+     * @dev Emitted when claiming is opened
+     * @param deadline The epoch time until which claiming is open
+     */
     event ClaimingOpened(uint256 deadline);
+
+    /**
+     * @dev Emitted when a user claims their funds
+     * @param user The address of the user claiming their funds
+     * @param pointsAccumulated The number of points accumulated by the user
+     */
     event Fundsclaimed(address indexed user, uint256 indexed pointsAccumulated);
 
-    /// @notice Allows ether to be sent to this contract
+    /**
+     * @dev Allows ether to be sent to this contract
+     */
     receive() external payable {}
 
-    //--------------------------------------------------------------------------------------
-    //----------------------------------  CONSTRUCTOR   ------------------------------------
-    //--------------------------------------------------------------------------------------
-
-    /// @notice Sets state variables needed for future functions
-    /// @param __rETH address of the rEth contract to receive
-    /// @param __wstETH address of the wstEth contract to receive
-    /// @param __sfrxETH address of the sfrxEth contract to receive
-    /// @param __cbETH address of the cbEth contract to receive
+    /**
+     * @dev Sets state variables needed for future functions
+     * @param __rETH Address of the rEth contract
+     * @param __wstETH Address of the wstEth contract
+     * @param __sfrxETH Address of the sfrxEth contract
+     * @param __cbETH Address of the cbEth contract
+     */
     constructor(
         IERC20 __rETH,
         IERC20 __wstETH,
@@ -86,14 +139,11 @@ contract RefactoredEarlyAdopterPool is Ownable, ReentrancyGuard, Pausable {
         isSupportedToken[__cbETH] = true;
     }
 
-    //--------------------------------------------------------------------------------------
-    //----------------------------  STATE-CHANGING FUNCTIONS  ------------------------------
-    //--------------------------------------------------------------------------------------
-
-    /// @notice deposits ERC20 tokens into contract
-    /// @dev User must have approved contract before
-    /// @param _erc20Contract erc20 token contract being deposited
-    /// @param _amount amount of the erc20 token being deposited
+    /**
+     * @dev Deposits ERC20 tokens into contract
+     * @param _erc20Contract ERC20 token contract being deposited
+     * @param _amount Amount of the ERC20 token being deposited
+     */
     function deposit(
         IERC20 _erc20Contract,
         uint256 _amount
@@ -112,7 +162,11 @@ contract RefactoredEarlyAdopterPool is Ownable, ReentrancyGuard, Pausable {
         emit DepositERC20(msg.sender, _amount);
     }
 
-    /// @notice deposits Ether into contract
+    /**
+     * @dev Deposits Ether into the contract
+     * @dev Emits a `DepositEth` event when the deposit is successful
+     * The amount of Ether being deposited, must be between 0.1 ether and 100 ether
+     */
     function depositEther()
         external
         payable
@@ -128,16 +182,21 @@ contract RefactoredEarlyAdopterPool is Ownable, ReentrancyGuard, Pausable {
         emit DepositEth(msg.sender, msg.value);
     }
 
-    /// @notice withdraws all funds from pool for the user calling
-    /// @dev no points allocated to users who withdraw
+    /**
+     * @dev Withdraws all funds from the pool for the user calling
+     * @dev Emits a `Withdrawn` event when the withdrawal is successful
+     */
     function withdraw() public nonReentrant {
         require(depositInfo[msg.sender].depositTime != 0, "No deposit stored");
         _transferFunds(0);
         emit Withdrawn(msg.sender);
     }
 
-    /// @notice Transfers users funds to a new contract such as LP
-    /// @dev can only call once receiver contract is ready and claiming is open
+    /**
+     * @dev Transfers users funds to a new contract such as LP
+     * @dev Can only be called once the receiver contract is ready and claiming is open
+     * @dev Emits a `Fundsclaimed` event when the transfer is successful
+     */
     function claim() public nonReentrant {
         require(claimingOpen == 1, "Claiming not open");
         require(
@@ -153,8 +212,11 @@ contract RefactoredEarlyAdopterPool is Ownable, ReentrancyGuard, Pausable {
         emit Fundsclaimed(msg.sender, pointsRewarded);
     }
 
-    /// @notice Sets claiming to be open, to allow users to claim their points
-    /// @param _claimDeadline the amount of time in days until claiming will close
+    /**
+     * @dev Sets claiming to be open, to allow users to claim their points
+     * @dev Emits a `ClaimingOpened` event when claiming is opened
+     * @param _claimDeadline The amount of time in days until claiming will close
+     */
     function setClaimingOpen(uint256 _claimDeadline) public onlyOwner {
         claimDeadline = block.timestamp + (_claimDeadline * 86400);
         claimingOpen = 1;
@@ -163,8 +225,11 @@ contract RefactoredEarlyAdopterPool is Ownable, ReentrancyGuard, Pausable {
         emit ClaimingOpened(claimDeadline);
     }
 
-    /// @notice Set the contract which will receive claimed funds
-    /// @param _receiverContract contract address for where claiming will send the funds
+    /**
+     * @dev Sets the contract which will receive claimed funds
+     * @dev Emitsa `ClaimReceiverContractSet` event when the receiver contract is set
+     * @param _receiverContract The contract address for where claimed funds will be sent
+     */
     function setClaimReceiverContract(
         address _receiverContract
     ) public onlyOwner {
@@ -174,8 +239,11 @@ contract RefactoredEarlyAdopterPool is Ownable, ReentrancyGuard, Pausable {
         emit ClaimReceiverContractSet(_receiverContract);
     }
 
-    /// @notice Calculates how many points a user currently has owed to them
-    /// @return the amount of points a user currently has accumulated
+    /**
+     * @notice Calculates how many points a user currently has owed to them
+     * @param _user The address of the user to calculate the points for
+     * @return The amount of points a user currently has accumulated
+     */
     function calculateUserPoints(address _user) public view returns (uint256) {
         uint256 lengthOfDeposit;
 
@@ -201,22 +269,24 @@ contract RefactoredEarlyAdopterPool is Ownable, ReentrancyGuard, Pausable {
             1e14;
     }
 
-    //Pauses the contract
+    /**
+     * @notice Pauses the contract
+     */
     function pauseContract() external onlyOwner {
         _pause();
     }
 
-    //Unpauses the contract
+    /**
+     * @notice Unpauses the contract
+     */
     function unPauseContract() external onlyOwner {
         _unpause();
     }
 
-    //--------------------------------------------------------------------------------------
-    //--------------------------------  INTERNAL FUNCTIONS  --------------------------------
-    //--------------------------------------------------------------------------------------
-
-    /// @notice Transfers funds to relevant parties and updates data structures
-    /// @param _identifier identifies which contract function called the function
+    /**
+     * @notice Transfers funds to relevant parties and updates data structures
+     * @param _identifier Identifies which contract function called the function
+     */
     function _transferFunds(uint256 _identifier) internal {
         UserDepositInfo storage userInfo = depositInfo[msg.sender];
 
@@ -245,11 +315,15 @@ contract RefactoredEarlyAdopterPool is Ownable, ReentrancyGuard, Pausable {
         payable(receiver).transfer(ethBalance);
     }
 
-    //--------------------------------------------------------------------------------------
-    //-------------------------------------     GETTERS  ------------------------------------
-    //--------------------------------------------------------------------------------------
-
-    /// @dev Returns the balance for each currencies, eth & TVL of contract
+    /**
+     * @dev Returns the balance for each currency, ether, and total value locked (TVL) of contract
+     * @return rETHBal The balance of rETH held by the contract
+     * @return wstETHBal The balance of wstETH held by the contract
+     * @return sfrxETHBal The balance of sfrxETH held by the contract
+     * @return cbETHBal The balance of cbETH held by the contract
+     * @return ethBal The balance of ether held by the contract
+     * @return tvl The total value locked in the contract
+     */
     function getContractTVL()
         public
         view
@@ -270,6 +344,16 @@ contract RefactoredEarlyAdopterPool is Ownable, ReentrancyGuard, Pausable {
         tvl = rETHBal + wstETHBal + sfrxETHBal + cbETHBal + ethBal;
     }
 
+    /**
+     * @dev Returns the balance for each currency, ether, and total value locked (TVL) of a specific user
+     * @param _user The address of the user to get the TVL for
+     * @return rETHBal The balance of rETH held by the user
+     * @return wstETHBal The balance of wstETH held by the user
+     * @return sfrxETHBal The balance of sfrxETH held by the user
+     * @return cbETHBal The balance of cbETH held by the user
+     * @return ethBal The balance of ether held by the user
+     * @return totalBal The total value locked by the user
+     */
     function getUserTVL(
         address _user
     )
@@ -290,22 +374,5 @@ contract RefactoredEarlyAdopterPool is Ownable, ReentrancyGuard, Pausable {
         cbETHBal = userToErc20Balance[_user][_cbETH];
         ethBal = depositInfo[_user].etherBalance;
         totalBal = (rETHBal + wstETHBal + sfrxETHBal + cbETHBal + ethBal);
-    }
-
-    //--------------------------------------------------------------------------------------
-    //-------------------------------------  MODIFIERS  ------------------------------------
-    //--------------------------------------------------------------------------------------
-
-    modifier onlyCorrectAmount(uint256 _amount) {
-        require(
-            _amount >= 0.1 ether && _amount <= 100 ether,
-            "Incorrect Deposit Amount"
-        );
-        _;
-    }
-
-    modifier depositingOpen() {
-        require(claimingOpen == 0, "Depositing closed");
-        _;
     }
 }
